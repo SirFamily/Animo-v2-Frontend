dc649e6639d2e8952d0973fd6b67f2ee86af124d
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2024-07-10 01:39:34 +0700

    "Refactored Navbar component to use useAuth hook and conditional rendering for user and guest navigation"

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,39 +1,41 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 import navcss from "./navcss/nav.module.css";
-const guestNav = [
-  {
-    text: (
-      <>
-        <div>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </div>
-      </>
-    ),
-  },
-];
 
-const userNav = [
-  {
-    text: (
-      <>
-        <ul>
-          <li>
-            <Link to="/dashboard/pet">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/setting">Settings</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </>
-    ),
-  },
-];
 function Navbar() {
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
+    const hdlLogout = () => {
+      logout();
+      navigate("/");
+    };
+  const guestNav = [
+    {
+      text: (
+        <>
+          <div className={navcss.container_nav_reg_log}>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        </>
+      ),
+    },
+  ];
+  
+  const userNav = [
+    {
+      text: (
+        <>
+        <div>{user?.email}</div>
+  
+              <button onClick={hdlLogout} >Logout</button>
+    
+        </>
+      ),
+    },
+  ];
+
   return (
     <div className={navcss.container}>
       <nav className={navcss.container_nav}>
@@ -45,9 +47,16 @@ function Navbar() {
           </Link>
         </div>
 
-        <div className={navcss.container_nav_reg_log}>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+        <div>
+          {user?.id ? (
+            <>
+              {userNav.map((item, index) => (
+                <div key={index}>{item.text}</div>
+              ))}
+            </>
+          ) : (
+            guestNav.map((item, index) => <div key={index}>{item.text}</div>)
+          )}
         </div>
       </nav>
     </div>
