fdac50cab3a55ee5150e5df422911508d4d17491
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2024-07-10 02:07:23 +0700

    "Added user router and conditional routing based on user authentication"

---
+++ src/pages/dashboard/pets/Pets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pets() {
+  return (
+    <div>
+      pet
+    </div>
+  )
+}
+
+export default Pets
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,14 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
 import Nav from "../navbar/Navbar";
 
 import Home from "../pages/Home"
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
-
+import Dashboard from "../pages/dashboard/Dashboard"
+import Pets from "../pages/dashboard/pets/Pets"
 const guestRouter = createBrowserRouter([
   {
     path: "/",
@@ -27,8 +30,25 @@ const guestRouter = createBrowserRouter([
   },
 ]);
 
+const userRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Nav />
+          <Outlet />
+        </>
+      ),
+      children: [
+        { index: true, element: <Dashboard /> },
+        { path: 'dashboard/pets', element: <Pets />,},
+    
+      ]
+    },
+  ]);
 
 export default function AppRouter() {
-
-  return <RouterProvider router={guestRouter} />;
+  const { user } = useAuth();
+  const finalRouter = user?.id ? userRouter : guestRouter
+  return <RouterProvider router={finalRouter} />;
 }
