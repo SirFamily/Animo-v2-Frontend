1d8b29cbcd66552594ae19c3ef2227facddb2e89
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2024-07-09 20:30:39 +0700

    "Remove styles from App.css and index.css, simplify App.jsx, and add CSS module to Navbar.jsx"

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,50 +1,42 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import navcss from "./navcss/nav.module.css";
 const guestNav = [
-    {
-      text: (
-        <>
-          <div>
-            <Link to="/login">
-              Login
-            </Link>
-            <Link to="/register">
-              Register
-            </Link>
-          </div>
-        </>
-      ),
-    },
-  ];
+  {
+    text: (
+      <>
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      </>
+    ),
+  },
+];
 
-  const userNav = [
-    {
-      text: (
-        <>
-          
-              <ul>
-                <li>
-                  <Link to="/dashboard/pet">Dashboard</Link>
-                </li>
-                <li>
-                  <Link to="/dashboard/setting">Settings</Link>
-                </li>
-                <li>
-                  <button>
-                    Logout
-                  </button>
-                </li>
-              </ul>
-
-        </>
-      ),
-    },
-  ];
+const userNav = [
+  {
+    text: (
+      <>
+        <ul>
+          <li>
+            <Link to="/dashboard/pet">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/setting">Settings</Link>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </>
+    ),
+  },
+];
 function Navbar() {
   return (
-    <div>
-       <nav>
+    <div className={navcss.container}>
+      <nav className={navcss.container_nav}>
         <div>
           <Link to="/">
             <div>
@@ -53,18 +45,13 @@ function Navbar() {
           </Link>
         </div>
 
-        <div>
-            <Link to="/login">
-              Login
-            </Link>
-            <Link to="/register">
-              Register
-            </Link>
-          </div>
-
+        <div className={navcss.container_nav_reg_log}>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
