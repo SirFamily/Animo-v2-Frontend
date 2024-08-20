c4895349cabae8870652515a50937ff875ba2a75
Thiraphat.ch <famlacviee60@gmail.com>
2024-08-20 21:25:27 +0700

    "Added new CSS classes and updated Hosts.jsx component to use them"

--- src/pages/dashboard/host/Hosts.jsx
+++ src/pages/dashboard/host/Hosts.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import Menu from "../../../menu/Menu"
+import React from "react";
+import Menu from "../../../menu/Menu";
+import csslayer from "../dashboardcss/dashlayer.module.css";
 
 function host() {
   return (
-    <div>
-      <Menu/>
-      <h1>host</h1>
-      
+    <div className={csslayer.container}>
+      <Menu />
+      <div className={csslayer.container_g_layer_host}>
+        <div className={csslayer.container_layer_top_host}>
+         test
+        </div>
+        <div className={csslayer.container_layer_buttom_host}>
+          <div className={csslayer.container_in_button_host}>
+          test
+          </div>
+          <div className={csslayer.container_in_button_host}>test</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default host
+export default host;
