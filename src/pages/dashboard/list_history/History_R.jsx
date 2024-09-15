1679fdbe53d0ecb4c1d4d1aa8a2c1bd83d64f6c6
Thiraphat.ch <famlacviee60@gmail.com>
2024-09-15 13:48:06 +0700

    "Updated dashboard CSS and JSX files to add new styles and classes"

--- src/pages/dashboard/list_history/History_R.jsx
+++ src/pages/dashboard/list_history/History_R.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Menu from "../../../menu/Menu"
+import csslayer from "../dashboardcss/dashlayer.module.css";
 
 function history() {
   return (
-    <div>
-      <Menu/>
-      <h1>history</h1>   
+    <div className={csslayer.container}>
+    <Menu />
+    <div className={csslayer.container_g_layer}>
+      <div className={csslayer.container_layer_buttom_his}>
+        <h1>history</h1>
+      </div>
     </div>
+  </div>
   )
 }
 
--- src/pages/dashboard/list_request/Requests.jsx
+++ src/pages/dashboard/list_request/Requests.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Menu from "../../../menu/Menu"
+import csslayer from "../dashboardcss/dashlayer.module.css";
 
 function request() {
   return (
-    <div>
-      <Menu/>
-      <h1>request</h1>
+    <div className={csslayer.container}>
+      <Menu />
+      <div className={csslayer.container_g_layer}>
+        <div className={csslayer.container_layer_buttom_req}>
+          <h1>request</h1>
+        </div>
+      </div>
     </div>
   )
 }
