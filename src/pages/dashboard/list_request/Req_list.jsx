8233132863acc2cdad4c7c5cf616f87246381015
Thiraphat.ch <famlacviee60@gmail.com>
2024-09-17 14:11:03 +0700

    fixbug

--- src/pages/dashboard/list_request/Req_list.jsx
+++ src/pages/dashboard/list_request/Req_list.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./Css/req_list.module.css"; // Import CSS Module
+import styles from "./Css/req_list.module.css"; 
 import axios from "axios";
 import { Link } from 'react-router-dom';
 
