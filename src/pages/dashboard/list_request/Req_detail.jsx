8bc8cfa691f5713fbbe65c6f99aea763d566e1f8
Thiraphat.ch <famlacviee60@gmail.com>
2024-09-17 14:38:37 +0700

    "Deleted Req_list.module.css and req_detail.module.css, and updated imports in Req_list.jsx and Req_detail.jsx to reference new CSS files."

--- src/pages/dashboard/list_request/Req_detail.jsx
+++ src/pages/dashboard/list_request/Req_detail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import styles from "./Css/req_detail.module.css";
+import styles from "./Css/requests_detail.module.css";
 
 function Req_detail() {
   const { reqId } = useParams(); // ดึง reqId จาก URL
